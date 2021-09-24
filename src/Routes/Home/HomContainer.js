fa6accaac58ce2af79d5aa800e9df0d2d1567260
Paek Seungwoo <paekseungwoo@Paekui-Macmini.local>
2021-09-24 14:42:06 +0900

    #5.3 TV Container

--- src/Routes/Home/HomContainer.js
+++ src/Routes/Home/HomContainer.js
@@ -23,7 +23,7 @@ export default class extends React.Component{
             })
         } catch {
             this.setState({
-                error: "Can't find movies information."
+                error: "Can't find movie information."
             })
         } finally {
             this.setState({
@@ -34,7 +34,6 @@ export default class extends React.Component{
 
     render() {
         const { nowPlaying, upcoming, popular, error, loading } = this.state;
-        console.log(this.state);
         return (
             <HomePresenter
                 nowPlaying={nowPlaying} 
