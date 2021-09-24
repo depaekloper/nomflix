70f129e011586a86f5df0500bcbbbc111b5a1e6d
Paek Seungwoo <paekseungwoo@Paekui-Macmini.local>
2021-09-24 17:00:21 +0900

    #6.3 SearchPresenter Component

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,16 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "Components/Loader";
+import Section from "Components/Section";
 
-const SearchPresenter = (movieResults, tvResults, searchTerm, error, loading, handleSubmit) => null;
+const Contailner = styled.div`
+    padding: 0px 20px;
+`;
+
+const Form = styled.form`
+    margin-bottom: 50px;
+    width: 100%;
+`;
+
+const Input = styled.input`
+    all: unset;
+    font-size: 28px;
+    width: 100%;
+`;
+
+const SearchPresenter = ({movieResults, tvResults, searchTerm, error, loading, handleSubmit, updateTerm}) => (
+<Contailner>
+    <Form onSubmit={handleSubmit}>
+        <Input placeholder="Search Movies or Tv Shows..." value={searchTerm} onChange={updateTerm} />
+    </Form>
+    {loading ? <Loader /> : <>
+        {movieResults && movieResults.length > 0 && <Section title="Movie Results">{movieResults.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>}
+        {tvResults && tvResults.length > 0 && <Section title="TV Show Results">{tvResults.map(show => <span key={show.id}>{show.name}</span>)}</Section>}
+    </>}
+</Contailner>);
 
 SearchPresenter.protoTypes = {
-    movieResults: PropTypes.array,
+    tvResults: PropTypes.array,
     tvResults: PropTypes.array,
     error: PropTypes.string,
     loading: PropTypes.bool.isRequired,
     searchTerm: PropTypes.string,
-    handleSubmit: PropTypes.func.isRequired
+    handleSubmit: PropTypes.func.isRequired,
+    updateTerm: PropTypes.func.isRequired
 };
 
 export default SearchPresenter;
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -5,14 +5,14 @@ import Section from "Components/Section";
 import Loader from "Components/Loader";
 
 const Container = styled.div`
-    padding: 0px 10px;
+    padding: 0px 20px;
 `;
 
 const TVPresenter = ({topRated, popular, airingToday, error, loading}) => loading ? <Loader /> : 
     <Container>
-        {topRated && topRated.length > 0 && <Section title="Top Rated Shows">{topRated.map(show => show.name)}</Section>}
-        {popular && popular.length > 0 && <Section title="Popular Shows">{popular.map(show => show.name)}</Section>}
-        {airingToday && airingToday.length > 0 && <Section title="Airing Today Shows">{airingToday.map(show => show.name)}</Section>}
+        {topRated && topRated.length > 0 && <Section title="Top Rated Shows">{topRated.map(show => <span key={show.id}>{show.name}</span>)}</Section>}
+        {popular && popular.length > 0 && <Section title="Popular Shows">{popular.map(show => <span key={show.id}>{show.name}</span>)}</Section>}
+        {airingToday && airingToday.length > 0 && <Section title="Airing Today Shows">{airingToday.map(show => <span key={show.id}>{show.name}</span>)}</Section>}
     </Container>;
 
 TVPresenter.protoTypes = {
