6b01cc618cc74e94f52410eafb7b2fa02a3d4313
Paek Seungwoo <paekseungwoo@Paekui-Macmini.local>
2021-09-24 16:24:24 +0900

    #6.1 HomePresenter and Section Components

---
+++ src/Components/Section.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Contailner = styled.div`
+    :not(:last-child) {
+        margin-bottom: 50px;
+    }
+`;
+
+const Title = styled.span`
+    font-size:14px;
+    font-weight: 600;
+`;
+
+const Grid = styled.div`
+    margin-bottom: 25px;
+`;
+
+
+const Section = ({title, children}) => (
+    <Contailner>
+        <Title>{title}</Title>
+        <Grid>{children}</Grid>
+    </Contailner>
+);
+
+Section.protoTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
+}
+
+export default Section;
