f68586e92c442210c50c0ec8bf589c74c9e7649a
Paek Seungwoo <paekseungwoo@Paekui-Macmini.local>
2021-09-24 16:08:45 +0900

    #6.0 Presenter Structure

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,15 @@
-export default () => "Home";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const HomePresenter = (nowPlaying, popular, upcoming, error, loading) => null;
+
+HomePresenter.protoTypes = {
+    nowPlaying: PropTypes.array,
+    popular: PropTypes.array,
+    upcoming: PropTypes.array,
+    error: PropTypes.string,
+    loading: PropTypes.bool.isRequired
+};
+
+export default HomePresenter;
