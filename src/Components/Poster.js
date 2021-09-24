898ba082d88d7f79572eb695aa9aa3be423a3e7b
Paek Seungwoo <paekseungwoo@Paekui-Macmini.local>
2021-09-24 18:06:53 +0900

    #6.7 Poster Component part Two

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -3,27 +3,66 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Contailner = styled.div``;
+const Contailner = styled.div`
+    font-size: 12px;
+`;
 
-const ImageContainer = styled.div``;
+const Image = styled.div`
+  background-image: url(${props => props.bgUrl});
+  height: 180px;
+  background-size: cover;
+  border-radius: 4px;
+  background-position: center center;
+  transition: opacity 0.1s linear;
+`;
 
-const Image = styled.img``;
+const Rating = styled.span`
+    bottom: 5px;
+    right: 5px;
+    position: absolute;
+    opacity: 0;
+`;
 
-const Rating = styled.span``;
+const ImageContainer = styled.div`
+  margin-bottom: 5px;
+  position: relative;
+  &:hover {
+    ${Image} {
+      opacity: 0.3;
+    }
+    ${Rating} {
+      opacity: 1;
+    }
+  }
+`;
 
-const Title = styled.span``;
+const Title = styled.span`
+    display: block;
+    margin-bottom: 3px;
+`;
 
-const Year = styled.span``;
+const Year = styled.span`
+    font-size: 10px;
+    color: rgba(255, 255, 255, 0.5);
+`;
 
 
 const Poster = ({id, imageUrl, title, rating, year, isMovie = false}) => (
     <Link to={isMovie? `/move/${id}` : `/show/${id}`}>
         <Contailner>
             <ImageContainer>
-                <Image bgUrl={imageUrl}  />
+                <Image
+                    bgUrl={
+                        imageUrl
+                        ? `https://image.tmdb.org/t/p/w300${imageUrl}`
+                        : require("../assets/noPosterSmall.png")
+                    }
+                />
                 <Rating><span role="img" aria-label="rating">⭐️</span>{" "}{rating}/10</Rating>
             </ImageContainer>
-            <Title>{title}</Title>
+            <Title>
+                {title.length > 13 ? `${title.substring(0, 13)}...` : title}
+            </Title>
             <Year>{year}</Year>
         </Contailner>
     </Link>
